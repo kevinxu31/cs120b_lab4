2591fed77dedd1995bdb5a6f968b3bd42c22c1de
Luofeng Xu <lxu092@wch136-24.cs.ucr.edu>
2020-10-24 22:12:20 -0700

    Completed part 4

--- turnin/lxu092_lab4_part4.c
+++ turnin/lxu092_lab4_part4.c
@@ -1,7 +1,7 @@
 /*	Author: lxu092
  *  Partner(s) Name: Luofeng Xu
  *	Lab Section:022
- *	Assignment: Lab 4  Exercise 3
+ *	Assignment: Lab 4  Exercise 4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
